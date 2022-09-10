82e8d2dab50736680e14e9b05c490afde2b54eea
Bashaier AlMeshaileh <56195895+bkmushaileh@users.noreply.github.com>
2022-09-10 23:53:12 +0300

    Shorter way cuz why not

--- ex2.dart
+++ ex2.dart
@@ -1,19 +1,25 @@
 void main() {
-  print(calculate(60));
+  print(calculate(55));
 }
 
 String calculate(int number) {
-  var grade = "";
-  if (number >= 90) {
-    grade = "A";
-  } else if (number < 90 && number >= 80) {
-    grade = "B";
-  } else if (number < 80 && number >= 70) {
-    grade = "C";
-  } else if (number < 70 && number >= 60) {
-    grade = "D";
-  } else {
-    grade = "F";
-  }
-  return grade;
+  // var grade = "";
+  // if (number >= 90) {
+  //   grade = "A";
+  // } else if (number < 90 && number >= 80) {
+  //   grade = "B";
+  // } else if (number < 80 && number >= 70) {
+  //   grade = "C";
+  // } else if (number < 70 && number >= 60) {
+  //   grade = "D";
+  // } else {
+  //   grade = "F";
+  // }
+  // return grade;
+
+  if (number >= 90) return "A";
+  if (number >= 80) return "B";
+  if (number >= 70) return "C";
+  if (number >= 60) return "D";
+  return "F";
 }
